9aff8cfdb4b50150a70544f317454be9abdec118
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2022-06-16 16:00:41 -0400

    Wraps error

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use anyhow::{Context, Result};
 use clap::Parser;
 
 /// Search for a pattern in a file and display the lines that contain it.
@@ -9,12 +10,20 @@ struct Cli {
     path: std::path::PathBuf,
 }
 
-fn main() {
+fn main() -> Result<()> {
     let args = Cli::parse();
-    let content = std::fs::read_to_string(&args.path).expect("could not read file");
+    let content = std::fs::read_to_string(&args.path).with_context(|| {
+        format!(
+            "Error reading file: {}",
+            &args.path.into_os_string().into_string().unwrap(),
+        )
+    })?;
+
     for line in content.lines() {
         if line.contains(&args.pattern) {
             println!("{}", line);
         }
     }
+
+    return Ok(());
 }
