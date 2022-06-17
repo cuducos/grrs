e331409536f2a7b3d7a0129df8798e0b85a3f9f6
Eduardo Cuducos <4732915+cuducos@users.noreply.github.com>
2022-06-16 22:23:06 -0400

    Processes files async

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 use anyhow::{Context, Result};
+use camino::Utf8PathBuf;
 use std::fmt;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 pub struct Match {
-    line_number: usize,
-    content: String,
+    pub line_number: usize,
+    pub content: String,
 }
 
 impl fmt::Display for Match {
@@ -14,17 +15,32 @@ impl fmt::Display for Match {
     }
 }
 
-pub fn find_matches(path: &std::path::PathBuf, pattern: &str) -> Result<Vec<Match>> {
-    let file = File::open(path).with_context(|| format!("Error opening {}", path.display()))?;
+pub struct Matches {
+    pub path: String,
+    pub lines: Vec<Match>,
+}
+
+impl fmt::Debug for Matches {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{} has {} matche(s)", self.path, self.lines.len())
+    }
+}
+
+pub async fn find_matches(path: &Utf8PathBuf, pattern: &str) -> Result<Matches> {
+    let path_as_str = path.to_string();
+    let file = File::open(path).with_context(|| format!("Error opening {}", &path_as_str))?;
     let reader = BufReader::new(file);
-    let mut matches: Vec<Match> = Vec::new();
+    let mut matches = Matches {
+        path: path.to_string(),
+        lines: Vec::new(),
+    };
 
     for (idx, result) in reader.lines().enumerate() {
         let number = idx + 1;
         let line = result
-            .with_context(|| format!("Error reading line {} from {}", number, path.display()))?;
+            .with_context(|| format!("Error reading line {} from {}", number, &path_as_str))?;
         if line.contains(pattern) {
-            matches.push(Match {
+            matches.lines.push(Match {
                 line_number: number,
                 content: line,
             });
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-use anyhow::{Context, Result};
+use anyhow::Result;
+use camino::Utf8PathBuf;
 use clap::Parser;
+use log::warn;
 use std::io::{self, Write};
+use tokio::sync::mpsc;
 
-/// Search for a pattern in a file and display the lines that contain it.
+/// Search for a pattern in file(s) and display the lines that contain it.
 #[derive(Parser)]
 struct Cli {
     pattern: String,
@@ -11,28 +14,50 @@ struct Cli {
     paths: Vec<std::path::PathBuf>,
 }
 
-fn main() -> Result<()> {
+fn convert_paths_to_utf8(paths: Vec<std::path::PathBuf>) -> Vec<Utf8PathBuf> {
+    let mut results: Vec<Utf8PathBuf> = Vec::new();
+    let mut errors = false;
+    for path in paths {
+        match Utf8PathBuf::from_path_buf(path) {
+            Ok(value) => results.push(value),
+            _ => errors = true,
+        }
+    }
+    if errors {
+        warn!(
+            "Discarded some file paths because they had ivalid encoding. Valid file paths: {}",
+            results.iter().map(|p| p.to_string()).collect::<Vec<String>>().join(", ")
+        );
+    }
+    results
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let args = Cli::parse();
     let mut stdout = io::BufWriter::new(io::stdout());
+    let paths = convert_paths_to_utf8(args.paths);
+    let multiple_paths = paths.len() > 1;
+    let (tx, mut rx) = mpsc::channel(64);
 
-    for path in &args.paths {
-        if args.paths.len() > 1 {
-            writeln!(stdout, "{}:", path.display())?;
+    tokio::spawn(async move {
+        for path in &paths {
+            let matches = grrs::find_matches(path, &args.pattern).await;
+            tx.send(matches).await.unwrap();
+        }
+    });
+
+    while let Some(result) = rx.recv().await {
+        let matches = result?;
+        if multiple_paths {
+            writeln!(stdout, "{}:", matches.path)?;
         }
 
-        let matches = grrs::find_matches(path, &args.pattern).with_context(|| {
-            format!(
-                "Error searching for `{}` in {}",
-                &args.pattern,
-                &path.display()
-            )
-        })?;
-
-        for line in matches {
+        for line in matches.lines {
             writeln!(stdout, "{}", line)?;
         }
 
-        if args.paths.len() > 1 {
+        if multiple_paths {
             writeln!(stdout, "")?;
         }
     }
